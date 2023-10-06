65f236effbcab508382f3063f3f70e47cba25035
Sifatpal <143998236+Sifatpal@users.noreply.github.com>
2023-10-06 16:45:50 +1100

    Add files via upload

---
+++ 1.3.js
@@ -0,0 +1,10 @@
+new Vue({
+    el: '#app',
+    data: {
+        homeTitle: 'This is my Portfolio',
+        aboutTitle: 'Info',
+        aboutContent: 'I am in process of improving my skills in HTML, CSS and Vuejs.',
+        contactTitle: 'Reachout',
+        contactContent: 'Email: sifatsingh347@gmail.com'
+    }
+});
